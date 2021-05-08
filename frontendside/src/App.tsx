1562af8cb14c2488c38582296d711a6637173170
DangHoangGeo <dang.hoang.geo@gmail.com>
2021-05-08 19:25:49 +0900

    Backend/adminside (#15)
    * test frontend login for api call
    
    * dang: login testing for api call

--- frontendside/src/App.tsx
+++ frontendside/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import Main from './main/main'
 import Provinces from './admin/Provinces';
+import Login from "./components/auth/Login";
 
 function App() {
   return (
       <BrowserRouter>
           <Route exact path="/" component={Main}/>
+          <Route path="/login" component={Login} />
           <Route path="/admin/province" component={Provinces}/>
       </BrowserRouter>
   );
