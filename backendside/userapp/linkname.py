ee96526de71e96fadc6bd24b7d633d400120122c
San Nguyen <vinhsannguyen91@gmail.com>
2021-05-09 21:47:37 +0900

    chore: rename folders

--- backendside/userapp/linkname.py
+++
@@ -1,6 +0,0 @@
-
-#URL
-VCTJ_HOME = 'vietcatholic-home'
-
-#template path
-VCTJ_TEMP_HOME = 'userapp/home.html'
--- backendside/userapp/src/components/CardProfile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Menu = () => {
-  return (
-    <div className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"></div>
-  );
-};
-
-export default Menu;
