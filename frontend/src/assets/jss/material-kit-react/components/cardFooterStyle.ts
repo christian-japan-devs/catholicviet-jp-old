42879e6e324fee56bcdde3887688d801eb5a59f9
DangHoangGeo <dang.hoang.geo@gmail.com>
2021-06-04 14:16:52 +0900

    add material-kit react styles

---
+++ frontend/src/assets/jss/material-kit-react/components/cardFooterStyle.ts
@@ -0,0 +1,13 @@
+//Material-ui components
+import { makeStyles } from '@material-ui/core/styles';
+
+export const cardFooterStyle = makeStyles(() => ({
+  cardFooter: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    padding: "0.9375rem 1.875rem",
+  },
+}));
+
+export default cardFooterStyle;
